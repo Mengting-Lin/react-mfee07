9e7d305dadeaf706a574f067e7ce67e87e3be304
Mengting-Lin <64238735+Mengting-Lin@users.noreply.github.com>
2020-06-02 21:29:15 +0800

    Update index.js

--- 1.js-counter/index.js
+++ 1.js-counter/index.js
@@ -1,4 +1,12 @@
+// JS
+
 document.getElementById("add").addEventListener("click", function () {
     document.getElementById("total").innerHTML =
       +document.getElementById("total").innerHTML + 1;
   });
+
+//jQuery
+
+// $('#add').on('click', function(){
+//     $('#total').html(+$('#total').html() + 1);
+// });
